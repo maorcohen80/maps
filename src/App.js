ce623489eeb8aaf9589be4ace6eb1c56fdf84975
Maor Cohen <maor@retravel.io>
2021-07-14 20:15:25 +0300

    push

--- src/App.js
+++ src/App.js
@@ -4,26 +4,51 @@ import "./App.css";
 import { ReactBingmaps } from "react-bingmaps";
 import TextField from "@material-ui/core/TextField";
 
-const pushPins = [
-  {
-    location: [42.360081, -71.058884],
-    option: { color: "red" },
-    addHandler: { type: "click", callback: () => {} },
-  },
-];
-
-function App() {
+const App = () => {
   const [cords, setCords] = useState([42.360081, -71.058884]);
   const [readyMap, setReadyMap] = useState(false);
+  const [pins, addPins] = useState([]);
+  const [polyline, setPolyLines] = useState({
+    location: [],
+    option: { strokeColor: "red", strokeThickness: 2, strokeDashArray: [0, 0, 0, 0] },
+  });
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       setCords([position.coords.latitude, position.coords.longitude]);
     });
   }, []);
+  console.log(`cords`, cords);
 
-  const handleValChange = (e) => {
-    console.log(`e.target.value`, e.target.value);
+  const addPinToMap = (a, b) => {
+    let updatedPins = {
+      location: [a, b],
+      option: { color: "red" },
+      addHandler: {
+        type: "click",
+        callback: () => {
+          console.log("added");
+        },
+      },
+    };
+    console.log(`pins`, pins);
+    addPins([...pins, updatedPins]);
   };
+
+  const addPolyLine = (a, b) => {
+    let newLine = { ...polyline };
+    let line = [a, b];
+    newLine.location.push(line);
+    console.log(`newLine`, newLine);
+    setPolyLines(newLine);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addPinToMap(e.target[0].value, e.target[1].value);
+    addPolyLine(e.target[0].value, e.target[1].value);
+    console.log(`e.target.value`, e.target[0].value);
+  };
+
   return (
     <div className="App">
       <div
@@ -41,21 +66,36 @@ function App() {
           <ReactBingmaps
             bingmapKey="AjMjGzQrdjeJT8mzYG-7NECnEOQF0NjhcAXyT4Oy5CxAHBrvZwm8-G_Tk9hdG2wG"
             center={[cords[0], cords[1]]}
-            onMapReady={!readyMap ? setReadyMap(true) : null}
-            pushPins={pushPins}
+            onMapReady={setTimeout(() => {
+              setReadyMap(true);
+            }, 2000)}
+            pushPins={pins}
+            polyline={polyline}
           />
-          {readyMap && (
-            <div style={{ marginLeft: "20px", display: "flex", flexDirection: "column" }}>
-              <form noValidate autoComplete="off">
-                <TextField id="standard-basic1" label="enter latitude" onChange={handleValChange} />
-                <TextField id="standard-basic2" label="enter longitude" />
-              </form>
-            </div>
-          )}
         </div>
+        {readyMap && (
+          <div style={{ marginLeft: "20px", display: "flex", flex: 0.4, flexDirection: "row" }}>
+            <form onSubmit={handleSubmit} noValidate autoComplete="off">
+              <TextField id="standard-basic1" label="enter latitude" />
+              <TextField id="standard-basic2" label="enter longitude" />
+              <br />
+              <TextField type="submit" value="Submit" />
+            </form>
+          </div>
+        )}
+      </div>
+      <div style={{ marginLeft: "280px" }}>
+        {pins.length > 0 &&
+          pins.map((pin) => {
+            return (
+              <div>
+                lan: {pin.location[0]} longitude: {pin.location[1]}
+              </div>
+            );
+          })}
       </div>
     </div>
   );
-}
+};
 
 export default App;
